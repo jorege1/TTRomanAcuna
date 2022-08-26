f23d9a849a65f0960c28daa1968de74164bea56f
JorgeRoman <96397847+jorege1@users.noreply.github.com>
2022-08-25 22:30:35 -0400

    Agregue pagina home

--- tesis/urls.py
+++ tesis/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tesis.views import vista1Def
+from tesis import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vista1/', vista1Def)
+    path('', views.homePage, name='home'),
+    path('bienvenido/', views.vista1Def),
 ]
--- tesis/views.py
+++ tesis/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+def homePage(request):
+    return redirect(vista1Def)
 
 def vista1Def(request):
     return render(request, 'vista1.html')
